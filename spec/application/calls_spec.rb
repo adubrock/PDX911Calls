80c343bb20ac164aeb732d35d7cb3a4e088a7040
Andrew DuBrock <adubrock@comcast.net>
2013-12-18 17:39:25 -0800

    tweaked test

--- spec/application/calls_spec.rb
+++ spec/application/calls_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe 'index' do
   it 'displays the last 100 calls' do
-    (calls.count).should == 100    
+    (@calls.count).should == 100    
   end
 
   it 'has valid data' do
-    calls.should include("PP13000411200")
+    @calls.should include("PP13000411200")
   end
 end
