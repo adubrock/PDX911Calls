211ddbabd5a7b6bfc5382846469204ee51122827
Paul Ogden <ageoldpun@gmail.com>
2013-12-24 15:31:07 -0800

    Fixed some type name change fallout

--- spec/acceptance/calls_list_spec.rb
+++ spec/acceptance/calls_list_spec.rb
@@ -3,9 +3,9 @@ require './spec/spec_helper'
 
 describe 'homepage' do
   it 'should show a list of calls' do
-    Call.create! type: "PERSON CONTACT (86)", address: "19600 BLOCK OF NE GLISAN ST, GRESHAM, OR"
-    Call.create! type: "TRAFFIC STOP",        address: "SE 80TH AVE / SE GLADSTONE ST, PORTLAND, OR"
-    Call.create! type: "WARRANT",             address: "19100 BLOCK OF E BURNSIDE ST, GRESHAM, OR"
+    Call.create! call_type: "PERSON CONTACT (86)", address: "19600 BLOCK OF NE GLISAN ST, GRESHAM, OR"
+    Call.create! call_type: "TRAFFIC STOP",        address: "SE 80TH AVE / SE GLADSTONE ST, PORTLAND, OR"
+    Call.create! call_type: "WARRANT",             address: "19100 BLOCK OF E BURNSIDE ST, GRESHAM, OR"
 
     visit "/"
 
