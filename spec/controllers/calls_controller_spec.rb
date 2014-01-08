eadd9c62d2f80eee0b9bc104ffa18df01fc92128
Paul Ogden <ageoldpun@gmail.com>
2014-01-08 12:42:31 -0800

    Test markers portion of controller

--- spec/controllers/calls_controller_spec.rb
+++ spec/controllers/calls_controller_spec.rb
@@ -4,14 +4,16 @@ require './spec/spec_helper'
 describe CallsController do
 
   describe "#index" do
-    it "sets a calls instance variable" do
+    it "sets a calls and markers instance variables" do
       calls = [1,2,3]
+      maps = ["a","b","c"]
       Call.stub(paginate: calls)
-      Map.stub(markers: calls)
+      Map.stub(markers: maps)
 
       get :index
 
       assigns(:calls).should == calls
+      assigns(:markers).should == maps
     end
   end
 end
