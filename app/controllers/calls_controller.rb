80badb13daaf6c7a97c6e54b592612316c321c86
Paul Ogden <ageoldpun@gmail.com>
2014-01-11 11:16:37 -0800

    Created search acceptance test

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,7 +1,12 @@
 class CallsController < ApplicationController
 
   def index
-    @calls = Call.paginate(page: params[:page], per_page: 20)
-    @markers = Map.markers(@calls)
+    if params[:search]
+      @calls = Call.paginate(page: params[:page], per_page: 20).all(:conditions => ['call_type LIKE ?', "%#{params[:search]}%"])
+      @markers = Map.markers(@calls)
+    else
+      @calls = Call.paginate(page: params[:page], per_page: 20)
+      @markers = Map.markers(@calls)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 PDX911Calls::Application.routes.draw do
   resources :calls
   root 'calls#index'
+  match '/search', to: 'calls#index', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
