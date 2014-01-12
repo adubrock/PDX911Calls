9b396197410c4ab3122d69971ef3167f198e2051
Paul Ogden <ageoldpun@gmail.com>
2014-01-11 18:36:30 -0800

    Revert "added search model, fixed tests but rake/tests slow bc of googleapi pause"
    This reverts commit 88612e0a62a93b8ae1ed53aac6bc035d031fd581.

--- app/models/call.rb
+++ app/models/call.rb
@@ -4,13 +4,12 @@ require 'date'
 class Call < ActiveRecord::Base
   default_scope -> { order('updated_at DESC') }
   validates_uniqueness_of :call_id
-  reverse_geocoded_by :latitude, :longitude do |call,results|
-    if geo = results.first
-    call.zip = geo.postal_code
-    sleep 0.1
-    end
-  end
-  after_validation :reverse_geocode
+  # reverse_geocoded_by :latitude, :longitude do |call,results|
+  #   if geo = results.first
+  #   call.zip = geo.postal_code
+  #   end
+  # end
+  # after_validation :reverse_geocode
 
   def self.import_from_xml_uri(uri)
     doc = Nokogiri::XML(open(uri)).remove_namespaces!
@@ -29,7 +28,9 @@ class Call < ActiveRecord::Base
                   agency: agency,
                   updated_at: updated_at,
                   latitude: latitude,
-                  longitude: longitude,)
+                  longitude: longitude,
+                  zip: '')
+
     end
   end
 
--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -1,8 +1,5 @@
 VCR.configure do |c|
   #the directory where your cassettes will be saved
-  c.ignore_request do |request|
-    URI(request.uri).host == 'maps.googleapis.com'
-  end
   c.cassette_library_dir = 'spec/fixtures/cassettes'
   # your HTTP request service. You can also use fakeweb, webmock, and more
   c.hook_into :webmock
