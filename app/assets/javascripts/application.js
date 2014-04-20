6c234aeeb76b15a0ca91f15bb81bdd8b872e0efb
Paul Ogden <ageoldpun@gmail.com>
2014-04-19 22:11:05 -0700

    refactor.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,32 @@
-//= require underscore
-//= require gmaps/google
+google.maps.event.addDomListener(window, 'load', function initialize() {
+
+  var map = new google.maps.Map(document.getElementById("map"));
+  var bounds = new google.maps.LatLngBounds();
+  var focusedInfoWindow;
+
+  var markers = markerAttributes.map(function(attributes) {
+    var myLatlng = new google.maps.LatLng(attributes.lat, attributes.lng);
+    bounds.extend(myLatlng);
+
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: attributes.title
+    });
+
+    var infoWindow = new google.maps.InfoWindow({
+      content: attributes.infoWindow
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infoWindow.open(map,marker);
+      if(focusedInfoWindow) {
+        focusedInfoWindow.close();
+      }
+      focusedInfoWindow = infoWindow;
+    });
+  })
+
+  map.fitBounds(bounds);
+});
+
